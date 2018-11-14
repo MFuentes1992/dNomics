214150532d8f1a24e815106b99ef439e0b32ec90
Marco A. Fuentes <markfuentes2991@gmail.com>
2018-11-13 22:47:29 -0600

    Creating Tables report_header and line_item

--- app/src/main/java/com/example/markf/dnomics/DatabaseModel.java
+++ app/src/main/java/com/example/markf/dnomics/DatabaseModel.java
@@ -23,9 +23,12 @@ public class DatabaseModel extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String strQueryCountry = "CREATE TABLE country (country_alphaID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, country_alpha TEXT NOT NULL, name TEXT NOT NULL);";
-
+        String strQueryReportHeader = "CREATE TABLE report_header (reportID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, personID INTEGER NOT NULL, report_name TEXT NOT NULL, report_date TEXT NOT NULL, report_location TEXT NOT NULL);";
+        String strQueryReportLineItem = "CREATE TABLE line_item (lineitemID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, reportID INTEGER NOT NULL, lineitem_date TEXT NOT NULL, lineitem_total TEXT NOT NULL, lineitem_purpose TEXT NOT NULL, lineitem_from TEXT NOT NULL, lineitem_to TEXT NOT NULL, lineitem_merchant TEXT NOT NULL, lineitem_allocation TEXT NOT NULL);";
         String strQueryPerson = "CREATE TABLE person (personID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, surname TEXT NOT NULL, uniqueID TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL, country_alphaID INTEGER NOT NULL, birth_date TEXT NOT NULL, create_date TEXT NOT NULL, update_date TEXT NOT NULL, img_data BLOB, estatus INTEGER NOT NULL);";
 
+        db.execSQL(strQueryReportHeader);
+        db.execSQL(strQueryReportLineItem);
         db.execSQL(strQueryCountry);
         db.execSQL(strQueryPerson);
     }
